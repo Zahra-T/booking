58ade68a1c789dc7e81c64194a8cc627f7b0e737
Zahra-T <zahratehraninasab@gmail.com>
2020-02-09 12:36:03 +0330

    Startup.cs modified to use psql

--- src/Startup.cs
+++ src/Startup.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
-using System.Linq; using System.Threading.Tasks; using Microsoft.AspNetCore.Builder;
+using System.Linq; 
+using System.Threading.Tasks; using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
@@ -9,6 +10,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.EntityFrameworkCore;
+using Booking;
 
 
 namespace Booking
@@ -38,8 +40,6 @@ namespace Booking
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
