6acb0cbb9b583c9767b62c4c51543556ab1b6eb9
Zahra-T <zahratehraninasab@gmail.com>
2020-02-09 12:41:12 +0330

    database tables modified and migrations added.

--- src/Migrations/20200203160659_Booking.AppDbContext.v2.Designer.cs
+++ src/Migrations/20200203160659_Booking.AppDbContext.v2.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Booking;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace booking.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200203160659_Booking.AppDbContext.v2")]
+    partial class BookingAppDbContextv2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -25,6 +27,9 @@ namespace booking.Migrations
                         .ValueGeneratedOnAdd()
                         .HasColumnType("integer")
                         .HasAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+
+                    b.Property<int>("DisplayLength")
+                        .HasColumnType("integer");
 
                     b.Property<string>("Name")
                         .IsRequired()
---
+++ src/Migrations/20200203160659_Booking.AppDbContext.v2.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace booking.Migrations
+{
+    public partial class BookingAppDbContextv2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "show",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "DisplayLength",
+                table: "salon",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DisplayLength",
+                table: "salon");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "show",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 30);
+        }
+    }
+}
